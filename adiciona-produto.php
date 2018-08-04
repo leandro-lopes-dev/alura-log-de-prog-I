eecfe7218db973c594966857db4995e1498a19d0
Leandro Lopes <macuser@iMac.local>
2018-08-04 16:47:30 -0300

    first commit

---
+++ adiciona-produto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$nome = $_GET["nome"];
+$preco = $_GET["preco"]
+?>	
+<h2>Produto <?= $nome; ?> com o preço de <?= $preco; ?> adicionado com sucesso!</h2>
+
+</body>
+</html>
---
+++ produto-formulario.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+nome:
+<input type="text" name="nome"></br>
+Preco:
+<input type="number" name="preco"></br>
+<input type="submit" name="Cadastrar">
+</body>
+</html>
